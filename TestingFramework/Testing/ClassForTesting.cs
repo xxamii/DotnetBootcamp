81baf3d0b413992e33160669434f919d4a7a4cec
SSkelleRR <maximiakovliev@gmail.com>
2022-01-21 18:56:31 +0200

    Moved TestingFramework project

---
+++ TestingFramework/Testing/ClassForTesting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Attributes;
+using BLL.Helpers;
+
+namespace Testing
+{
+    [TestClass]
+    public class ClassForTesting
+    {
+        [Fact]
+        public void SomeTest_a_equals_b()
+        {
+            int a = 1;
+            int b = 1;
+
+            Assert.IsEqual(a, b);
+        }
+
+        [Fact]
+        public void SomeTest_a_does_not_equal_b()
+        {
+            int a = 1;
+            int b = 2;
+
+            Assert.IsEqual(a, b);
+        }
+    }
+}
